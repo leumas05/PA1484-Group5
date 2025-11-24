08f0730124218e374ade9ee58f7998f25c03d739
S4m <57343922+leumas05@users.noreply.github.com>
2025-11-24 01:39:10 +0100

    Reset WiFi credentials in credentials.h to default values

--- project/credentials.h
+++ project/credentials.h
@@ -1,2 +1,2 @@
-static const char* WIFI_SSID     = "Samuels s23+";
-static const char* WIFI_PASSWORD = "BTH12345";
+static const char* WIFI_SSID     = "SSID";
+static const char* WIFI_PASSWORD = "PWD";
