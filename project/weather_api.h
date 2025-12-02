5805c8c3781866c698247433e55bfed2fa82bf38
S4m <57343922+leumas05@users.noreply.github.com>
2025-12-02 17:48:25 +0100

    Revert "Merge branch 'main' of https://github.com/leumas05/PA1484-Group5"
    This reverts commit 60224ca1c38017371a1a902da004063d459ccda4, reversing
    changes made to daaa62be5a29c141499a19636e5f6c34aa5b56f9.

--- project/weather_api.h
+++ project/weather_api.h
@@ -10,27 +10,8 @@ float getCurrentTemperature(String& statusMsg);
 
 // Function to fetch current wind data from SMHI API
 // Returns wind speed in m/s or NAN if request fails
+// windDirection will contain wind direction in degrees (0-360)
 // statusMsg will contain a user-friendly status message
-float getCurrentWindSpeed(String& statusMsg);
-
-// Function to fetch current wind direction from SMHI API
-// Returns wind direction in degrees (0-360) or NAN if request fails
-// statusMsg will contain a user-friendly status message
-float getCurrentWindDirection(String& statusMsg);
-
-// Function to fetch current rainfall from SMHI API
-// Returns rainfall in mm or NAN if request fails
-// statusMsg will contain a user-friendly status message
-float getCurrentRain(String& statusMsg);
-
-// Function to fetch current humidity from SMHI API
-// Returns humidity in percentage (0-100) or NAN if request fails
-// statusMsg will contain a user-friendly status message
-float getCurrentHumidity(String& statusMsg);
-
-float getCurrentPressure(String& statusMsg);
-
-// Change the current station by name. Returns station number or -1 on error.
-int change_station_nr(const char* new_station);
+float getCurrentWind(String& statusMsg, float& windDirection);
 
 #endif
